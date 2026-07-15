8fb5544b13901b1f16034e584b4669e1781055ef
Danicka <danickao@sidefx.com>
2026-07-14 19:09:27 -0500

    [IGNORE] Startup scripts put in pythonrc.py instead of 456.py due to override issues

--- scripts/python/pythonrc.py
+++ scripts/python/pythonrc.py
@@ -1,5 +1,7 @@
-import hou
+import hou, labutils
 
+
+# Hide GSplat nodes in Houdini versions before 22.0, as they are not compatible
 if hou.applicationVersion()[0] < 22:
     try:
         hou.hscript("ophide Lop labs::relight_gsplats::1.0")
@@ -7,4 +9,8 @@ if hou.applicationVersion()[0] < 22:
         hou.hscript("ophide Sop labs::normals_from_gsplats::1.0")
     except hou.Error:
         pass
+
+
+# Deprecating Labs OCIO ACES Profile and Labs Alternative Grey Background for Houdini Versions 20 - 21
+labutils.deprecate_settings_warnings()
 
